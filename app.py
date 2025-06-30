1948db72135aa446af5b652396a3d498128b8552
Mohd shad <98746318+Mohdshaadsonofvakeel@users.noreply.github.com>
2025-06-30 19:22:13 +0530

    Milestone-VP Validation

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+    
+DATABASE = os.path.join("/tmp", "parking.db")
 
 def init_db():
     conn = get_db_connection()
